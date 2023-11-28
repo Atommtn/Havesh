bf46de4a954cb108f7307085ff35df64f84435f5
Mohammad <atom.mtn@gmail.com>
2023-11-28 17:07:47 +0330

    private class

---
+++ HaveshApp/Admin/Planning/PrivateTimesTableDialog.razor.cs
@@ -0,0 +1,155 @@
+﻿using Havesh.Model.Model;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Havesh.Domain.Services;
+using HaveshApp.Managment.Session;
+
+namespace HaveshApp.Admin.Planning;
+
+public partial class PrivateTimesTableDialog
+{
+
+	[CascadingParameter]
+	MudDialogInstance DialogInstance { get; set; }
+
+	[Parameter]
+	public ShokouhPardisTimeTable? TimeTableItem { get; set; }
+
+	[Inject] DataProviderService DataProvider { get; set; }
+	[Parameter]
+	public ShokouhPardisTermClass TermPram { get; set; }
+
+	[Inject]
+	public ISnackbar Snackbar { get; set; }
+	[Parameter]
+	public EventCallback<ShokouhPardisTermClass> TermPramChanged { get; set; }
+
+	List<ShokouhPardisTeacherTimeSheet>? _teacherTimeSheet;
+    [Inject] public IDialogService DialogService { get; set; }
+
+	ShokouhPardisLevelBookPrice? _bookPrice;
+	private ShokouhPardisClassRoom _classRoom;
+	private List<ShokouhPardisSchedule> _scheduleList;
+
+    Dictionary<int, bool> Days { get; set; } = new Dictionary<int, bool>();
+    protected override void OnAfterRender(bool firstRender)
+	{
+
+		if (firstRender)
+		{
+			TimeTableItem.Term = TermPram;
+			_teacherTimeSheet = GetTimeSheet(TimeTableItem);
+			_bookPrice = GetBookPrice(TimeTableItem);
+
+			StateHasChanged();
+		}
+		base.OnAfterRender(firstRender);
+	}
+
+	private void CloseDialogClick()
+	{
+		DialogInstance.Cancel();
+	}
+
+
+
+	private async Task SaveDialogClick()
+	{
+		await _form.Validate();
+		if (!_form.IsValid)
+		{
+			// if (TimeTableItem?.ClassRoom == null)
+			// {
+			//     _form.Errors.ForEach(x => Snackbar.Add(x , Severity.Info));
+			//     //Snackbar.Add("کلاس باید انتخاب شود.", Severity.Error);
+			// }
+
+			return;
+		}
+
+		DialogInstance.Close(DialogResult.Ok(TimeTableItem));
+	}
+
+    //void SaveTemplate()
+    //{
+    //    var x = string.Join(',', Days.Where(x => x.Value == true).Select(x => x.Key));
+    //    TermSessionTemplate.WeekdayIds = x;
+    //    _dataProvider.SaveTermSessionTemplate(TermSessionTemplate);
+    //    Log.Warning("User {UserName} Save TermSessionTemplate {TermSessionTemplateId}", _userSession.Payload.UserName, TermSessionTemplate.Id);
+    //    _templates = DataProviderService.GetTermTemplates(Term);
+    //    TermSessionTemplate = new TermSessionTemplate();
+    //    StateHasChanged();
+
+    //}
+
+    ShokouhPardisClassRoom ClassRoom
+	{
+		get => _classRoom;
+		set
+		{
+			_classRoom = value;
+		}
+	}
+
+	ShokouhPardisLevelClass Level
+	{
+		get
+		{
+			return TimeTableItem.Level;
+
+		}
+		set
+		{
+			TimeTableItem.Level = value;
+			_bookPrice = GetBookPrice(TimeTableItem);
+		}
+	}
+
+	public List<ShokouhPardisSchedule> ScheduleList
+	{
+		get
+		{
+			_scheduleList = DataProvider.GetSchedules(TermPram);
+			return _scheduleList;
+		}
+		set => _scheduleList = value;
+	}
+
+
+	List<ShokouhPardisTeacherTimeSheet>? GetTimeSheet(ShokouhPardisTimeTable? timeTable)
+	{
+		if (timeTable is { Term: { }, Teacher: { } })
+		{
+			return DataProvider.GetTermTeacherTimeSheets(
+				timeTable.Term.Id,
+				timeTable.Teacher.Id);
+		}
+		return null;
+	}
+
+	private ShokouhPardisLevelBookPrice? GetBookPrice(ShokouhPardisTimeTable? timeTable)
+	{
+		if (timeTable.Level is { })
+		{
+			return DataProvider.GetLevelBookPrice(timeTable);
+
+		}
+		return null;
+	}
+    public TermSessionTemplate TermSessionTemplate { get; set; } 
+    private MudForm _form;
+    private async Task DateChoiceClick()
+    {
+        var x = string.Join(',', Days.Where(x => x.Value == true).Select(x => x.Key));
+        
+        TermSessionTemplate = new TermSessionTemplate()
+        {
+			TermFk = TermPram.Id,
+			WeekdayIds = x
+        };
+    await DialogService.ShowAsync<PrivateTimeTableDatesDialog>("Dates", new DialogParameters()
+        {
+            ["TermSessionTemplate"] = TermSessionTemplate
+    });
+    }
+}
